8a5c2720f76659d77b96f9a6ad963f0757ef511a
lloyd aaron <lloydaaron.barnacha@yahoo.com>
2016-02-29 12:33:14 +0800

    added script

---
+++ script.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+  $('.add-to-cart').click(function() {
+    var cart = $('.cart');
+    var item = $(this).parents(".product-container").find("img");
+    if(item){
+      var itemClone = item.clone()
+        .offset({
+          top: item.offset().top,
+          left: item.offset().left
+        })
+        .css({
+          'position': 'absolute',
+          'height': item.height(),
+          'width': item.width(),
+          'z-index': '2000',
+          'opacity': '0.7'
+        })
+        .appendTo($('body'))
+        .animate({
+          'top': cart.offset().top + 10,
+          'left': cart.offset().left + 30,
+          'width': item.width() / 10,
+          'height': item.height()/10
+        }, 1000);
+      itemClone.animate({
+        'width': 0,
+        'height': 0
+      }, function() {
+        $(this).detach()
+      });
+    }
+  });
+});
